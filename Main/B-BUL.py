1bf9c45cb4e05360bffedf9792c0dca95702c036
BernardoWilgenhoff <bernardowilgenhoff@live.com>
2022-10-29 09:45:01 -0300

    B-BUL.py
    Programa para comprar bulones

---
+++ Main/B-BUL.py
@@ -0,0 +1,27 @@
+# Variables
+
+from ast import While
+
+
+diametro = "1/4" , "3/16", "1/2", "5/8", "7/8", "3/4", "1"
+
+largo = "1/4", "3/16", "1/2", "3/4", "1"
+
+terminacion = "negro", "zincado"
+
+cantidad_requerida = input()
+
+# El usuario debe ingresar los atributos del bulon
+# para verificar si el mismo esta disponible
+
+#Ingresa los datos y verifica que los mismos son correctos"
+
+print("Buen día! Gracias por elegirnos")
+print("Seleccione el bulon que desea comprar")
+
+diametro_input = input("Introduzca el diametro requerido  ")
+
+if diametro_input == diametro:
+    print("Introduzca el largo requerido ")
+else:
+    print("Error")
