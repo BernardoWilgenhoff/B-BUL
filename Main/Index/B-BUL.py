ffbbb70ea8a135a5868fea008cc79cb76cee4abe
BernardoWilgenhoff <bernardowilgenhoff@live.com>
2022-10-29 18:59:31 -0300

    Rev.B
    Agregue una sub-rutina que debería ejecutarse hasta que la salida sea "y"
    
    No se como hacer para que vuelva a empezar, porque cuando coloco "n" finaliza la ejecución
    
    Sigo intentando

--- Main/Index/B-BUL.py
+++ Main/Index/B-BUL.py
@@ -1,7 +1,9 @@
 # Variables
 
 from ast import While
+from cgitb import reset
 from operator import concat
+from tkinter import N, Y
 
 diametro = 1, 2, 3, 4, 5
 
@@ -16,32 +18,64 @@ cantidad_requerida = input()
 
 #Ingresa los datos y verifica que los mismos son correctos"
 
+def verificacion_parametros():
+    while True:
+        diametro_input = int(input("Introduzca el diametro requerido: "))
+        if diametro_input != 1:
+            print("Pone 1")
+        else:
+            print("Pusiste 1")
+            break
+        continue
+
+    while True:
+        largo_input = int(input("Introduzca el largo requerido: "))
+        if largo_input != 10:
+            print("Pone 10")
+        else:
+            print("Pusiste 10")
+            break
+            continue
+
+    while True:
+        terminacion_input = (input("Introduzca la terminación requerida: "))
+        if terminacion_input != "negro":
+            print("Pone negro")
+        else:
+            print("Pusiste negro")
+            break
+        continue
+     
+    print("Usted selecciono un bulón:")
+    print(("Diametro:  ") + str(diametro_input))
+    print(("Largo:  ") + str(largo_input))
+    print("Terminacion:" + str(terminacion_input))
+
+    print("Si el bulón que selecciono es correcto presione y")
+    print("De no ser correcto presione n")
+    paso_1 = input(("Desea continuar "))
+
+
+    if paso_1 == "y":
+        cantidad_requerida()
+    else:
+        return
+            
+
+def cantidad_requerida():
+    while True:
+        cantidad_requerida = int(input("Introduzca la cantidad requerida: "))
+        if cantidad_requerida > 0:
+            print("OK")
+        else:
+            print("Debe colocar un numero positivo")
+            break
+            continue
+
 print("Buen día! Gracias por elegirnos")
 print("Seleccione el bulon que desea comprar")
 
-while True:
-    diametro_input = int(input("Introduzca el diametro requerido: "))
-    if diametro_input != 1:
-        print("Pone 1")
-    else:
-        print("Pusiste 1")
-        break
-    continue
+verificacion_parametros()
 
-while True:
-    largo_input = int(input("Introduzca el largo requerido: "))
-    if largo_input != 10:
-        print("Pone 10")
-    else:
-        print("Pusiste 10")
-        break
-    continue
 
-while True:
-    terminacion_input = (input("Introduzca la terminación requerida: "))
-    if terminacion_input != "negro":
-        print("Pone negro")
-    else:
-        print("Pusiste negro")
-        break
-    continue
+
